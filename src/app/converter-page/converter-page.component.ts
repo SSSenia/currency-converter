c1cb7fedfddbf539ae36123a26b0c38c7e5d8996
Senia <82032813+SSSenia@users.noreply.github.com>
2023-02-01 20:40:58 +0200

    feature: initial project setup

---
+++ src/app/converter-page/converter-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-converter-page',
+  templateUrl: './converter-page.component.html',
+  styleUrls: ['./converter-page.component.scss']
+})
+export class ConverterPageComponent {
+
+}
