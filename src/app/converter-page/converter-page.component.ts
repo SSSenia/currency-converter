dc2e5e5df0075e54ae7b2f0d0c3f505f78cabf4f
Senia <82032813+SSSenia@users.noreply.github.com>
2023-02-02 08:51:43 +0200

    bugfix: Simplified calculation architecture

--- src/app/converter-page/converter-page.component.ts
+++ src/app/converter-page/converter-page.component.ts
@@ -10,15 +10,11 @@ import { ParseApiService } from '../shared/parse-api.service';
   styleUrls: ['./converter-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ConverterPageComponent implements OnInit, OnDestroy {
+export class ConverterPageComponent implements OnInit {
 
   public selectCurrency: FormControl = new FormControl('USD');
   public customCurrency: FormControl = new FormControl(0);
   public hryvnia: FormControl = new FormControl(0);
-
-  public subToSelectCurrency!: Subscription;
-  public subToCustomCurrency!: Subscription;
-  public subToHryvnia!: Subscription;
 
   public exchange$!: Observable<ICurrency[]>;
   private exchange: ICurrency[] = [];
@@ -30,31 +26,22 @@ export class ConverterPageComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.exchange$ = this.parseApiService.getExchange().pipe(
       tap((exchange) => this.exchange = exchange));
-
-    this.subToSelectCurrency = this.selectCurrency.valueChanges.subscribe((value) => {
-      const hryvnia: number = this.customCurrency.value * this.exchange.find(x => x.cc === value)!.rate
-      if (Math.round(hryvnia * 10000) !== Math.round(this.hryvnia.value * 10000))
-        this.hryvnia.setValue(hryvnia);
-    })
-
-    this.subToCustomCurrency = this.customCurrency.valueChanges.subscribe((value) => {
-      const hryvnia: number = value * this.exchange.find(x => x.cc === this.selectCurrency.value)!.rate
-      if (Math.round(hryvnia * 10000) !== Math.round(this.hryvnia.value * 10000))
-        this.hryvnia.setValue(hryvnia);
-    })
-
-    this.subToHryvnia = this.hryvnia.valueChanges.subscribe((value) => {
-      const customCurrency: number = value / this.exchange.find(x => x.cc === this.selectCurrency.value)!.rate;
-      if (Math.round(customCurrency * 10000) !== Math.round(this.customCurrency.value * 10000))
-        this.customCurrency.setValue(customCurrency);
-    })
-
   }
 
-  ngOnDestroy(): void {
-    this.subToSelectCurrency.unsubscribe()
-    this.subToCustomCurrency.unsubscribe()
-    this.subToHryvnia.unsubscribe()
+  public calculateHryvnia() {
+    if (+this.customCurrency.value)
+      this.hryvnia.setValue(Math.round((this.customCurrency.value * this.exchange.find(x => x.cc === this.selectCurrency.value)!.rate) * 100) / 100);
+    else this.resetValues();
   }
 
+  public calculateCustomCurrency() {
+    if (+this.hryvnia.value)
+      this.customCurrency.setValue(Math.round((this.hryvnia.value / this.exchange.find(x => x.cc === this.selectCurrency.value)!.rate) * 100) / 100);
+    else this.resetValues();
+  }
+
+  public resetValues() {
+    this.customCurrency.setValue(0);
+    this.hryvnia.setValue(0);
+  }
 }
