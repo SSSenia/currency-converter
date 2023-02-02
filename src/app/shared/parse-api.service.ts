a7758a12af37c9a774bcd26e04ff8f96e5aeb74a
Senia <82032813+SSSenia@users.noreply.github.com>
2023-02-02 15:19:24 +0200

    feature: saving exchange on service

--- src/app/shared/parse-api.service.ts
+++ src/app/shared/parse-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { ICurrency } from './interfaces';
 
 @Injectable({
@@ -14,7 +14,10 @@ export class ParseApiService {
     private http: HttpClient
   ) { }
 
-  public getExchange(): Observable<ICurrency[]>{
-    return this.exchange ? of(this.exchange) : this.http.get<ICurrency[]>('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+  public getExchange(): Observable<ICurrency[]> {
+    return this.exchange
+      ? of(this.exchange)
+      : this.http.get<ICurrency[]>('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+        .pipe(tap((exchange) => this.exchange = exchange));
   }
 }
